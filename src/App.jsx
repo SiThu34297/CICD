7f306d97adb27e6420a0375673d313128a690e6f
SiThu34297 <sthu34297@gmail.com>
2023-07-23 23:49:49 +0630

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from './pages/home';
+
+function App() {
+    return (
+        <>
+            <h1>Hello</h1>
+            <h1>Develop</h1>
+            <Home />
+        </>
+    );
+}
+
+export default App;
